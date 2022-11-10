e5b15c2f6adaa15390d426696d26b40bee50fc67
Amanda Campos <amandaolivcampos@gmail.com>
2022-11-10 20:10:56 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
+    }
+}
