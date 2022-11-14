68d47d0dd8b63740196f8304ed5560a884ec299e
Amanda Campos <amandaolivcampos@gmail.com>
2022-11-14 13:29:06 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessException extends RuntimeException {
+import boardgame.BoardException;
+
+public class ChessException extends BoardException {
 
     public ChessException(String msg) {
         super(msg);
