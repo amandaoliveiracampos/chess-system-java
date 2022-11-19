3f1de35d734db4d815759c47c705cd9824a6c616
Amanda Campos <amandaolivcampos@gmail.com>
2022-11-19 14:41:01 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -45,6 +45,8 @@ public class Program {
             }
 
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
 
     }
 }
