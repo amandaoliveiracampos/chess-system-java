54552184f8992729f7721d00580ed7e2dccddf27
Amanda Campos <amandaolivcampos@gmail.com>
2022-11-20 16:21:46 -0300

    Queen

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -57,7 +57,7 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColumn()] = true;
 
             }
-            p.setValues(position.getRow() + 1, position.getColumn() + 1);
+            p.setValues(position.getRow() + 1, position.getColumn() - 1);
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
 
